3e3fc8f0e5baab27079c9aab812b97168eb98bd8
mdejournett <mdejournett2@huskers.unl.edu>
2023-03-17 19:23:17 -0500

    adding spawning of pinpoints at hand with air tap

--- MORTI2223/Assets/PinPlacer.cs
+++ MORTI2223/Assets/PinPlacer.cs
@@ -7,6 +7,9 @@ using UnityEngine.InputSystem;
 
 public class PinPlacer : MonoBehaviour
 {
+    private bool canSpawn = true;
+    private float SpawnTimer = 0;
+    private float MaxTimeForSpawn = 3;
     public GameObject objectToSpawn;
     [SerializeField]
     private GameObject leftHand;
@@ -17,24 +20,47 @@ public class PinPlacer : MonoBehaviour
     private InputActionReference leftHandReference;
     [SerializeField]
     private InputActionReference rightHandReference;
-    
+    Vector3 currentLeftLocation;
+    Vector3 currentRightLocation;
     private void ProcessRightHand(InputAction.CallbackContext ctx)
     {
         ProcessHand(ctx, rightHand);
-        Instantiate(objectToSpawn);
+        if (canSpawn)
+        {
+            canSpawn = false;
+            print("trying to instanciate");
+            Instantiate(objectToSpawn).transform.position = rightHand.transform.position;
+        }
     }
 
     private void ProcessLeftHand(InputAction.CallbackContext ctx)
     {
         ProcessHand(ctx, leftHand);
-        Instantiate(objectToSpawn);
+        if (canSpawn)
+        {
+            canSpawn = false;
+            print("trying to instanciate");
+            Instantiate(objectToSpawn).transform.position = leftHand.transform.position;
+        }
     }
 
     private void ProcessHand(InputAction.CallbackContext ctx, GameObject g)
     {
         g.SetActive(ctx.ReadValue<float>() > 0.95f);
     }
-
+    private void Update()
+    {
+        if (!canSpawn && (SpawnTimer < MaxTimeForSpawn))
+        {
+            SpawnTimer += Time.deltaTime;
+        }
+        else
+        {
+            SpawnTimer = 0;
+            canSpawn = true;
+        }
+        
+    }
     private void Start()
     {
         leftHand.SetActive(false);
