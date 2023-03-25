bb36fc09f850640367a6403d2ff2152b223abb1d
Peyton Comer <pcomer2@huskers.unl.edu>
2023-03-25 11:29:52 -0500

    HUD fix?

--- MORTI2223/Assets/Scripts/Data Displays/TimeCheck.cs
+++ MORTI2223/Assets/Scripts/Data Displays/TimeCheck.cs
@@ -24,49 +24,31 @@ public class TimeCheck : MonoBehaviour
         waterTime.SetActive(false);
         oxygenTime.SetActive(false);
         
-        // battery = Convert.ToDouble(TeleLIB.getTBattery());
-        // oxygen = Convert.ToDouble(TeleLIB.getTOxygen());
-        // water = Convert.ToDouble(TeleLIB.getTWater());
-
-        // if ((battery < oxygen) && (battery < water)){
-        //     oxygenTime.SetActive(false);
-        //     waterTime.SetActive(false);
-        //     batteryTime.SetActive(true);
-        // }
-        // else if((oxygen < battery) && (oxygen < water)){
-        //     batteryTime.SetActive(false);
-        //     waterTime.SetActive(false);
-        //     oxygenTime.SetActive(true);
-        // }
-        // else if((water < battery) && (water < oxygen)){
-        //     batteryTime.SetActive(false);
-        //     oxygenTime.SetActive(false);
-        //     waterTime.SetActive(true);
-        // }
+        
     }
 
     void Update()
     {
-        battery = Convert.ToDouble(TeleLIB.getTBattery());
-        oxygen = Convert.ToDouble(TeleLIB.getTOxygen());
-        water = Convert.ToDouble(TeleLIB.getTWater());
-
-
-        // if ((battery < oxygen) && (battery < water)){
-        //     oxygenTime.SetActive(false);
-        //     waterTime.SetActive(false);
-        //     batteryTime.SetActive(true);
-        // }
-        // if((oxygen < battery) && (oxygen < water)){
-        //     batteryTime.SetActive(false);
-        //     waterTime.SetActive(false);
-        //     oxygenTime.SetActive(true);
-        // }
-        // if((water < battery) && (water < oxygen)){
-        //     batteryTime.SetActive(false);
-        //     oxygenTime.SetActive(false);
-        //     waterTime.SetActive(true);
-            
+        battery = TimeToInt.TimeToSec(TeleLIB.getTBattery());
+        oxygen = TimeToInt.TimeToSec(TeleLIB.getTOxygen());
+        water = TimeToInt.TimeToSec(TeleLIB.getTWater());
         
-    }
+
+        if ((battery < oxygen) && (battery < water)){
+            oxygenTime.SetActive(false);
+            waterTime.SetActive(false);
+            batteryTime.SetActive(true);
+        }
+        if((oxygen < battery) && (oxygen < water)){
+            batteryTime.SetActive(false);
+            waterTime.SetActive(false);
+            oxygenTime.SetActive(true);
+        }
+        if((water < battery) && (water < oxygen)){
+            batteryTime.SetActive(false);
+            oxygenTime.SetActive(false);
+            waterTime.SetActive(true);
+            
+         }
+}
 }
--- MORTI2223/Assets/Scripts/Data Displays/WarningTextChange.cs
+++ MORTI2223/Assets/Scripts/Data Displays/WarningTextChange.cs
@@ -17,7 +17,7 @@ public class WarningTextChange : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        WarningSymbol.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
