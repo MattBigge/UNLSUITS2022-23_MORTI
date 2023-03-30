0c18e4bc91d2ddb6d89d8f106344195547adeb98
j-seibel <107728907+j-seibel@users.noreply.github.com>
2023-03-30 11:07:26 -0500

    Lat and Lon acess

---
+++ MORTI2223/Assets/Telemetry/Scripts/Position.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Position : MonoBehaviour
+{
+
+    [SerializeField]
+    float lon =0;
+    [SerializeField]
+    float lat =0;
+    // Start is called before the first frame update
+
+    public float[] getPos()
+    {
+        return new float[] { lat, lon };
+    }
+
+    public void setPos(string json)
+    {
+        JsonUtility.FromJsonOverwrite(json, this);
+    }
+    // Update is called once per frame
+    
+}
