469735fb01a4d4514d83fd0c14278977693c5e81
mdejournett <mdejournett2@huskers.unl.edu>
2023-03-30 20:17:31 -0500

    adding double tap to spawn

--- MORTI2223/Assets/Navigation/PinPlacer.cs
+++ MORTI2223/Assets/Navigation/PinPlacer.cs
@@ -10,15 +10,15 @@ public class PinPlacer : MonoBehaviour
     public bool PlaceDisable = false;
     public List<GameObject> PinList = new List<GameObject>(); 
     private bool canSpawn = false;
+    private float currentTime = 0;
     private float SpawnTimer = 0;
-    private float MaxTimeForSpawn = 3;
+    private float MaxTimeForSpawn = 0;
     public GameObject objectToSpawn;
+    private int frameCounter = 0;
     [SerializeField]
     private GameObject leftHand;
     [SerializeField]
     private GameObject rightHand;
-    private bool isRightPinch= false;
-    private bool isLeftPinch=false;
     [SerializeField]
     private InputActionReference leftHandReference;
     [SerializeField]
@@ -27,95 +27,31 @@ public class PinPlacer : MonoBehaviour
     Vector3 currentRightLocation;
     private void ProcessRightHand(InputAction.CallbackContext ctx)
     {
-        print("im being held down omg help");
-        if (isRightPinch)
+        currentTime = Time.time;
+        if (currentTime - SpawnTimer <= 3 && currentTime - MaxTimeForSpawn > 3)
         {
-            isRightPinch = false;
-
-        }
-        else
-        {
-            isRightPinch = true;
-            StartCoroutine(pinchRightUpdate());
-        }
-        
-    }
-    private IEnumerator pinchRightUpdate()
-    {
-        int i = 0;
-        while (isRightPinch)
-        {
-            i++;
-            yield return new WaitForSeconds(1);
-        }
-        if (i >= 3)
-        {
-            if (canSpawn&&!PlaceDisable)
-            {
-                canSpawn = false;
-                PinList.Add(Instantiate(objectToSpawn));
-                PinList[PinList.Count - 1].transform.position = currentRightLocation;
-            }
-        }
-    }
-    private IEnumerator pinchLeftUpdate()
-    {
-        int i = 0;
-        while (isLeftPinch)
-        {
-            i++;
-            yield return new WaitForSeconds(1);
-        }
-        if (i >= 3)
-        {
-            if (canSpawn && !PlaceDisable)
-            {
-                canSpawn = false;
-                print(currentLeftLocation);
-                PinList.Add(Instantiate(objectToSpawn));
-                PinList[PinList.Count - 1].transform.position = currentLeftLocation;
-            }
-        }
-    }
-    private void ProcessLeftHand(InputAction.CallbackContext ctx)
-    {
-        if (isLeftPinch)
-        {
-            isLeftPinch = false;
-
-        }
-        else
-        {
-            isLeftPinch = true;
-            StartCoroutine(pinchLeftUpdate());
-        }
-        
-        //ProcessHand(ctx, leftHand);
-    } 
-
-    private void ProcessHand(InputAction.CallbackContext ctx, GameObject g)
-    {
-        g.SetActive(ctx.ReadValue<float>() > 0.95f);
-    }
-    private void Update()
-    {
-        currentRightLocation = rightHand.transform.position;
-        currentLeftLocation = leftHand.transform.position;
-        if (!canSpawn && (SpawnTimer < MaxTimeForSpawn))
-        {
-            SpawnTimer += Time.deltaTime;
-        }
-        else
-        {
-            SpawnTimer = 0;
             canSpawn = true;
         }
-        
+        else
+        {
+            SpawnTimer = Time.time;
+        }
+        if (canSpawn)
+        {
+            MaxTimeForSpawn = Time.time;
+            currentRightLocation = rightHand.transform.position;
+            canSpawn = false;
+            PinList.Add(Instantiate(objectToSpawn));
+            PinList[PinList.Count - 1].transform.position = currentRightLocation;
+        }
     }
+     
+    private void ProcessLeftHand(InputAction.CallbackContext ctx)
+    {
+    } 
     private void Start()
     {
-        //leftHand.SetActive(false);
-        //rightHand.SetActive(false);
+
         leftHandReference.action.performed += ProcessLeftHand;
         rightHandReference.action.performed += ProcessRightHand;
     }
