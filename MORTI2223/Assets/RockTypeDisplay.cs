937256505812733f37546ca61b5873a25246d70a
Elijah Schoneweis <89532300+MrE42@users.noreply.github.com>
2023-05-09 19:51:59 -0500

    Display Updates Off Of inputs

--- MORTI2223/Assets/RockTypeDisplay.cs
+++ MORTI2223/Assets/RockTypeDisplay.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RockTypeDisplay : MonoBehaviour
 {
     // Start is called before the first frame update
     
-    public GameObject Si;
-    public GameObject Ti;
-    public GameObject Al;
-    public GameObject Fe;
-    public GameObject Mn;
-    public GameObject Mg;
-    public GameObject Ca;
-    public GameObject KO;
-    public GameObject PO;
+    public GameObject SampleNumber;
+    public GameObject Mission;
+    public GameObject RockType;
+    public GameObject Petrology;
+    public GameObject SiO2;
+    public GameObject TiO2;
+    public GameObject Al2O3;
+    public GameObject FeO;
+    public GameObject MnO;
+    public GameObject MgO;
+    public GameObject CaO;
+    public GameObject K2O;
+    public GameObject P2O3;
 
     public bool display = false;
     
@@ -28,11 +33,75 @@ public class RockTypeDisplay : MonoBehaviour
     private float podStartMovement = 0;
     public float transportMP = 0;
     
+    public List<string> SampleNumbers = new List<string>();
+    public List<int> Missions = new List<int>();
+    public List<string> RockTypes = new List<string>();
+    public List<string> Petrologies = new List<string>();
+    public List<float> SiO2s = new List<float>();
+    public List<float> TiO2s = new List<float>();
+    public List<float> Al2O3s = new List<float>();
+    public List<float> FeOs = new List<float>();
+    public List<float> MnOs = new List<float>();
+    public List<float> MgOs = new List<float>();
+    public List<float> CaOs = new List<float>();
+    public List<float> K2Os = new List<float>();
+    public List<float> P2O3s = new List<float>();
+    
+    public float inputSiO2;
+    public float inputTiO2;
+    public float inputAl2O3;
+    public float inputFeO;
+    public float inputMnO;
+    public float inputMgO;
+    public float inputCaO;
+    public float inputK2O;
+    public float inputP2O3;
+
+    public int sample;
     void Start()
     {
         transport = gameObject.GetComponent<RectTransform>();
         transportUp = new Vector2(transport.anchoredPosition.x, transport.anchoredPosition.y);
         transportDown = new Vector2(transport.anchoredPosition.x+transportOffset, transport.anchoredPosition.y);
+        
+        // Sample Numbers
+        SampleNumbers.AddRange(new string[] { "70,215,312", "15556,11", "12,002,492", "14,310,220", "12052,26", "15555,62", "10017,30" });
+
+        // Missions
+        Missions.AddRange(new int[] { 17, 15, 12, 14, 12, 15, 11 });
+
+        // Rock Types
+        RockTypes.AddRange(new string[] { "Mare basalt", "Vesicular basalt", "Olivine basalt", "Feldspathic basalt", "Pigeonite basalt", "Olivine basalt", "Ilmenite basalt" });
+
+        // Petrologies
+        Petrologies.AddRange(new string[] { "Fine-grained, porphyritic", "Medium-grained, small olv phenocrysts", "Medium-grained, porphyritic", "Fine-grained, phenos of plag, and pyx", "Porphyritic, phenos of oliv and cpx", "Medium-grained, phenos of oliv and pyx", "Fine-grainded, vesicular, poikilitic" });
+
+        // SiO2
+        SiO2s.AddRange(new float[] { 40.58f, 36.89f, 41.62f, 46.72f, 46.53f, 42.45f, 42.56f });
+
+        // TiO2
+        TiO2s.AddRange(new float[] { 12.83f, 2.44f, 2.44f, 1.1f, 3.4f, 1.56f, 9.38f });
+
+        // Al2O3
+        Al2O3s.AddRange(new float[] { 10.91f, 9.6f, 9.52f, 19.01f, 11.68f, 11.44f, 12.03f });
+
+        // FeO
+        FeOs.AddRange(new float[] { 13.18f, 14.52f, 18.12f, 7.21f, 16.56f, 17.91f, 11.27f });
+        
+        // MnO
+        MnOs.AddRange(new float[] { 0.19f, 0.24f, 0.27f, 0.14f, 0.24f, 0.27f, 0.17f });
+
+        // MgO
+        MgOs.AddRange(new float[] { 6.7f, 5.3f, 11.1f, 7.83f, 6.98f, 10.45f, 9.7f });
+
+        // CaO
+        CaOs.AddRange(new float[] { 10.64f, 8.22f, 8.12f, 14.22f, 11.11f, 9.37f, 10.52f });
+
+        // K2O
+        K2Os.AddRange(new float[] { -0.11f, -0.13f, -0.12f, 0.43f, -0.02f, -0.08f, 0.28f });
+
+        // P2O3
+        P2O3s.AddRange(new float[] { 0.34f, 0.29f, 0.28f, 0.65f, 0.38f, 0.34f, 0.44f });
     }
 
     // Update is called once per frame
@@ -48,5 +117,64 @@ public class RockTypeDisplay : MonoBehaviour
 
         transportMP = Mathf.Clamp(transportMP, 0, 1); 
         transport.anchoredPosition = Vector2.Lerp(transportDown, transportUp, transportMP);
+        
+        int closestSampleIndex = FindClosestSampleIndex();
+        sample = closestSampleIndex;
+        
+        if (sample >= 0 && sample < SampleNumbers.Count)
+        {
+            SampleNumber.GetComponent<TextMeshProUGUI>().text = "Sample Number: " + SampleNumbers[sample];
+            Mission.GetComponent<TextMeshProUGUI>().text = "Mission: " + Missions[sample].ToString();
+            RockType.GetComponent<TextMeshProUGUI>().text = "Rock Type: " + RockTypes[sample];
+            Petrology.GetComponent<TextMeshProUGUI>().text = "Petrology: " + Petrologies[sample];
+            SiO2.GetComponent<TextMeshProUGUI>().text = "SiO2: " + SiO2s[sample].ToString();
+            TiO2.GetComponent<TextMeshProUGUI>().text = "TiO2: " + TiO2s[sample].ToString();
+            Al2O3.GetComponent<TextMeshProUGUI>().text = "Al2O3: " + Al2O3s[sample].ToString();
+            FeO.GetComponent<TextMeshProUGUI>().text = "FeO: " + FeOs[sample].ToString();
+            MnO.GetComponent<TextMeshProUGUI>().text = "MnO: " + MnOs[sample].ToString();
+            MgO.GetComponent<TextMeshProUGUI>().text = "MgO: " + MgOs[sample].ToString();
+            CaO.GetComponent<TextMeshProUGUI>().text = "CaO: " + CaOs[sample].ToString();
+            K2O.GetComponent<TextMeshProUGUI>().text = "K2O: " + K2Os[sample].ToString();
+            P2O3.GetComponent<TextMeshProUGUI>().text = "P2O3: " + P2O3s[sample].ToString();
+
+        }
+        else
+        {
+            display = false;
+        }
+    }
+    int FindClosestSampleIndex()
+    {
+        int closestSampleIndex = -1;
+        float smallestDistance = float.MaxValue;
+
+        for (int i = 0; i < SampleNumbers.Count; i++)
+        {
+            float distance = CalculateDistance(i);
+            if (distance < smallestDistance)
+            {
+                smallestDistance = distance;
+                closestSampleIndex = i;
+            }
+        }
+
+        return closestSampleIndex;
+    }
+
+    float CalculateDistance(int index)
+    {
+        float distance = 0;
+
+        distance += Mathf.Pow(inputSiO2 - SiO2s[index], 2);
+        distance += Mathf.Pow(inputTiO2 - TiO2s[index], 2);
+        distance += Mathf.Pow(inputAl2O3 - Al2O3s[index], 2);
+        distance += Mathf.Pow(inputFeO - FeOs[index], 2);
+        distance += Mathf.Pow(inputMnO - MnOs[index], 2);
+        distance += Mathf.Pow(inputMgO - MgOs[index], 2);
+        distance += Mathf.Pow(inputCaO - CaOs[index], 2);
+        distance += Mathf.Pow(inputK2O - K2Os[index], 2);
+        distance += Mathf.Pow(inputP2O3 - P2O3s[index], 2);
+
+        return Mathf.Sqrt(distance);
     }
 }
