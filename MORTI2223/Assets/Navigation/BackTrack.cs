99e42a67d57e4c3bede45fbaefed3b76d353ce36
Elijah Schoneweis <89532300+MrE42@users.noreply.github.com>
2023-05-11 19:38:38 -0500

    Fixes

--- MORTI2223/Assets/Navigation/BackTrack.cs
+++ MORTI2223/Assets/Navigation/BackTrack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BackTrack : MonoBehaviour
 {
-    public GameObject NavSystem
+    public GameObject NavSystem;
     // Start is called before the first frame update
     void Start()
     {
