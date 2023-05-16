e5b4a0685f8504b26c1e6d4f4e45f459de3b2cae
prestonward2022@gmail.com <prestonward2022@gmail.com>
2023-05-16 17:31:04 -0500

    added final menu prefab to prefab folder

--- MORTI2223/Assets/Telemetry/Scripts/SendRoverCords.cs
+++ MORTI2223/Assets/Telemetry/Scripts/SendRoverCords.cs
@@ -21,7 +21,7 @@ private string uri = "";
     }
     
 
-    void OnConfirm(){
+    public void OnConfirm(){
         float[] cords = new float[2];
         string[] split = coords.text.Split(',');
         cords[0] = float.Parse(split[0]);
