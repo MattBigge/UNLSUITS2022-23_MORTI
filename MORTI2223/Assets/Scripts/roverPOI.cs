920279ab82b68d37dd70683c315a07d898fd9d01
Elijah Schoneweis <89532300+MrE42@users.noreply.github.com>
2023-05-19 00:15:09 -0500

    D Fix

--- MORTI2223/Assets/Scripts/roverPOI.cs
+++ MORTI2223/Assets/Scripts/roverPOI.cs
@@ -17,7 +17,7 @@ public class roverPOI : MonoBehaviour
         coordinates.Add(new Vector2(29.5648150f, -95.0817410f)); // A
         coordinates.Add(new Vector2(29.5646824f, -95.0811564f)); // B
         coordinates.Add(new Vector2(29.5650460f, -95.0810944f)); // C
-        coordinates.Add(new Vector2(29.5645430f, -95.0516440f)); // D
+        coordinates.Add(new Vector2(29.5645430f, -95.0816440f)); // D
         coordinates.Add(new Vector2(29.5648290f, -95.0813750f)); // E
         coordinates.Add(new Vector2(29.5647012f, -95.0813750f)); // F
         coordinates.Add(new Vector2(29.5651359f, -95.0807408f)); // G
